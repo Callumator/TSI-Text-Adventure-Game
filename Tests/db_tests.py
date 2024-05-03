5fa4b06fea7620129cbae3ffa081f7646a254764
Callum Smith <2905031S@student.gla.ac.uk>
2024-05-03 15:23:10 +0100

    db test (faking of database)

--- Tests/db_tests.py
+++ Tests/db_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from ReadDB import *
+from src.ReadDB import database_manager
 
 class TestDB(unittest.TestCase):
 
@@ -7,4 +7,15 @@ class TestDB(unittest.TestCase):
         self.db = database_manager(":memory:")
 
     def tearDown(self):
-        self.db.tearDown()
+        self.db.close_connection()
+
+    def test_insert_user(self):
+        self.db.insert_user('email1', 'Fname1', 'Lname1', 'password1')
+        self.db.insert_user('email2', 'Fname2', 'Lname2', 'password2')
+        Customers = self.db.get_users()
+        self.assertEqual(len(Customers), 2)
+        self.assertEqual(Customers[0][1], 'email1')
+        self.assertEqual(Customers[1][1], 'email2')
+
+if __name__ == '__main__':
+    unittest.main()
